9fef58dcbf2ee333eab54dcd7ac8ced2d7df141d
shuisheng <2274751790@qq.com>
2022-11-17 00:26:34 +0800

    1

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-console.log("hello");
+console.log("aa-crlf");
 console.log(123);
 console.log(1111);
 console.log(222);
---
+++ b.js
@@ -0,0 +1,5 @@
+console.log("bbbb-crlf");
+console.log(123);
+console.log(1111);
+console.log(222);
+console.log(33322222);
